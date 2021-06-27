3a89c4e0eb7e1a810cbb6fedfa725948c0ae503f
coloradope <coloradope@HBOGCOLORADOPE.HILDEBOG.LOCAL>
2021-06-26 21:57:08 -0500

    Initial commit

---
+++ alphabetSoup/src/jsopaletras_v02/Main.java
@@ -0,0 +1,15 @@
+
+package jsopaletras_v02;
+/**
+ * @author Edmond Duke
+ * email: edu1738@gmail.com
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        frmCreaSopaLetras frm = new frmCreaSopaLetras();
+        frm.setVisible(true);
+    }
+    //Notas: Error al agregar palabra de 1 letra
+}
+
